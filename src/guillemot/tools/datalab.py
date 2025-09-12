a8048b8cc1aeea2ba45b7eaeeeaa29851116605d
Matthew Evans <git@ml-evs.science>
2025-09-12 22:09:45 +0100

    Linting

--- src/guillemot/tools/datalab.py
+++ src/guillemot/tools/datalab.py
@@ -3,12 +3,14 @@ import os
 from pathlib import Path
 from pydantic_ai import ModelRetry
 
+
 def get_samples():
     """Get a list of all samples available in the Datalab
     and list whether they have XRD data."""
     client = DatalabClient("demo.datalab-org.io")
     samples = client.get_items("samples")
     return samples
+
 
 def get_sample(sample_id: str | None):
     """Get a sample with the given ID and download its associated data files."""
@@ -24,7 +26,7 @@ def get_sample(sample_id: str | None):
     finally:
         os.chdir(str(orig_dir))
 
+
 def list_data_files(sample_id: str):
     """List data files that have been downloaded for a given sample."""
     return os.listdir(f"./data/{sample_id}")
-
